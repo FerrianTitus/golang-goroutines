f70190445ecf1133fe6b25c996f53fe88c4f805c
Ferrian <ferrian00@gmail.com>
2022-11-16 22:02:37 +0700

    goroutine test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ferriantitus/golang-goroutines
+
+go 1.17
---
+++ goroutine_test.go
@@ -0,0 +1,29 @@
+package golang_goroutines
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func RunHelloWorld() {
+	fmt.Println("Hello World")
+}
+
+func TestCreateGoroutine(t *testing.T) {
+	go RunHelloWorld()
+	fmt.Println("Test")
+
+	time.Sleep(1 * time.Second)
+}
+
+func DisplayNumber(number int) {
+	fmt.Println("Display", number)
+}
+
+func TestDisplayNumber(t *testing.T) {
+	for i := 0; i < 100000; i++ {
+		go DisplayNumber(i)
+	}
+	time.Sleep(5 * time.Second)
+}
